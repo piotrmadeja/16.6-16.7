4ac399afc62e6e0ac4522936f1e8f4483e8b735c
piotrmadeja <36596744+piotrmadeja@users.noreply.github.com>
2018-05-28 19:11:56 +0200

    fix format

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,13 +2,12 @@ import React from 'react';
 import style from './Title.css';
 
 const Title = (props) => {
-	return (
-	<div className={style.Title}>
-		<h1></h1>
-		<p>Liczba zadań: {props.number}</p>
-	</div>
-	);
-	
+  return (
+    <div className={style.Title}>
+      <h1></h1>
+      <p>Liczba zadań: {props.number}</p>
+    </div>
+  );
 }
 
 export default Title;
