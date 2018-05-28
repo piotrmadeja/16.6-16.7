b65532d5ff36d92e695506704f73fc878e354615
piotrmadeja <36596744+piotrmadeja@users.noreply.github.com>
2018-05-28 19:12:50 +0200

    fix format

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './Todo.css';
 
 const Todo = (props) => {
-	return (
-		<li className={style.TodoList}>
-			<a href={'#'}>
-				<p>{props.todo.text}</p>
-				<button onClick={() => props.remove(props.todo.id)}>x</button>
-			</a>
-		</li>
-	);
+  return (
+    <li className={style.TodoList}>
+      <a href={'#'}>
+        <p>{props.todo.text}</p>
+	<button onClick={() => props.remove(props.todo.id)}>x</button>
+      </a>
+    </li>
+  );
 }
 
 export default Todo;
